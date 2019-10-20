c009c31a213a042a9000d76bffda923f49b02883
uniquexiaobai <uniquexiaobai@gmail.com>
2019-10-20 22:45:00 +0800

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const fetchDataFromTophub = (url) => async () => {
 	const { data } = await axios.get(url);
