42d5b37b8112d94de86a97e9629bc5476d358844
uniquexiaobai <baihang@bytedance.com>
2021-02-15 18:34:33 +0800

    fix xinquji

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const app = express();
 const cache = {};
 
 const fetchDataFromTophub = url => async () => {
-	const { data } = await axios.get(url);
-	const $ = cheerio.load(data);
-	const list = [];
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  const list = [];
 
-	$('.Zd-p-Sc .cc-dc:first-child table tr').each(function() {
-		const $a = $(this).find('td[class=al] > a');
-		list.push({
-			id: $a.attr('itemid'),
-			title: $a.text().trim(),
-			url: $a.attr('href'),
-		});
-	});
+  $('.Zd-p-Sc .cc-dc:first-child table tr').each(function () {
+    const $a = $(this).find('td[class=al] > a');
+    list.push({
+      id: $a.attr('itemid'),
+      title: $a.text().trim(),
+      url: $a.attr('href'),
+    });
+  });
 
-	return list;
+  return list;
 };
 
 const weiboTophubUrl = 'https://tophub.today/n/KqndgxeLl9';
@@ -40,149 +40,149 @@ const fetch36kyData = fetchDataFromTophub(_36kyTophubUrl);
 const fetchSspaiData = fetchDataFromTophub(sspaiTopHubUrl);
 
 const fetchEchojsData = async () => {
-	const { data } = await axios.get('https://www.echojs.com/');
-	const $ = cheerio.load(data);
-	const list = [];
+  const { data } = await axios.get('https://www.echojs.com/');
+  const $ = cheerio.load(data);
+  const list = [];
 
-	$('#newslist > article').each(function() {
-		const $a = $(this).find('h2 > a');
-		const id = $(this).data('news-id');
-		const title = $a.text();
-		const url = $a.attr('href');
+  $('#newslist > article').each(function () {
+    const $a = $(this).find('h2 > a');
+    const id = $(this).data('news-id');
+    const title = $a.text();
+    const url = $a.attr('href');
 
-		list.push({
-			id,
-			title,
-			url,
-		});
-	});
+    list.push({
+      id,
+      title,
+      url,
+    });
+  });
 
-	return list;
+  return list;
 };
 
 const fetchYuqueData = async () => {
-	const baseUrl = 'https://www.yuque.com';
-	const { data } = await axios.get(
-		`${baseUrl}/api/explore/recommends?limit=20`
-	);
-	const list = data.data.docs.map(item => {
-		return {
-			id: item.id,
-			title: item.title,
-			url: `${baseUrl}/${item.book.user.login}/${item.book.slug}/${item.slug}`,
-		};
-	});
+  const baseUrl = 'https://www.yuque.com';
+  const { data } = await axios.get(
+    `${baseUrl}/api/explore/recommends?limit=20`
+  );
+  const list = data.data.docs.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      url: `${baseUrl}/${item.book.user.login}/${item.book.slug}/${item.slug}`,
+    };
+  });
 
-	return list;
+  return list;
 };
 
 const fetchMaoyanData = async () => {
-	const baseUrl = 'https://piaofang.maoyan.com';
-	const { data } = await axios.get(`${baseUrl}/rankings/year`);
-	const $ = cheerio.load(data);
-	const list = [];
+  const baseUrl = 'https://piaofang.maoyan.com';
+  const { data } = await axios.get(`${baseUrl}/rankings/year`);
+  const $ = cheerio.load(data);
+  const list = [];
 
-	$('#ranks-list > .row').each(function() {
-		const id = $(this)
-			.data('com')
-			.match(/\/(\d+)\'/)[1];
-		const title = $(this)
-			.find('.first-line')
-			.text();
-		const url = `${baseUrl}/movie/${id}`;
+  $('#ranks-list > .row').each(function () {
+    const id = $(this)
+      .data('com')
+      .match(/\/(\d+)\'/)[1];
+    const title = $(this).find('.first-line').text();
+    const url = `${baseUrl}/movie/${id}`;
 
-		list.push({
-			id,
-			title,
-			url,
-		});
-	});
+    list.push({
+      id,
+      title,
+      url,
+    });
+  });
 
-	return list;
+  return list;
 };
 
-const fetchXinqujiData = async () => {
-	const baseUrl = 'https://xinquji.com';
-	const fetchUrl = `${baseUrl}/frontend/post/groups?cursor=0`;
-	const { data } = await axios.get(fetchUrl);
-	const list = data.data.map(item => {
-		return {
-			id: item.id,
-			title: item.name,
-			desc: item.description,
-			url: `${baseUrl}/posts/${item.id}`,
-		};
-	});
-
-	return list;
+const fetchXinqujiData = async (query = {}) => {
+  const range = parseInt(query.range ?? 7);
+  const baseUrl = 'https://xinquji.com';
+  const urls = Array.from(
+    { length: range },
+    (_, i) => `${baseUrl}/frontend/post/groups?cursor=${i}`
+  );
+  const resList = await Promise.all(urls.map(url => axios.get(url)));
+  const dataList = resList.map(res => res?.data?.data);
+  const list = dataList.flat(2).map(item => ({
+    id: item.id,
+    title: item.name,
+    desc: item.description,
+    url: `${baseUrl}/posts/${item.id}`,
+  }));
+  return list;
 };
 
 const targetActionMap = {
-	weibo: fetchWeiboData, // 微博
-	weixin: fetchWeixinData, // 微信
-	kaiyan: fetchKaiyanData, // 开眼
-	zhihu: fetchZhihuData, // 知乎日报
-	zuimei: fetchZuimeiData, // 最美应用
-	echojs: fetchEchojsData, // Echojs
-	yuque: fetchYuqueData, // 语雀
-	maoyan: fetchMaoyanData, // 猫眼
-	'36ky': fetch36kyData, // 36氪
-	sspai: fetchSspaiData, // 少数派
-	xinquji: fetchXinqujiData, // 新趣集
+  weibo: fetchWeiboData, // 微博
+  weixin: fetchWeixinData, // 微信
+  kaiyan: fetchKaiyanData, // 开眼
+  zhihu: fetchZhihuData, // 知乎日报
+  zuimei: fetchZuimeiData, // 最美应用
+  echojs: fetchEchojsData, // Echojs
+  yuque: fetchYuqueData, // 语雀
+  maoyan: fetchMaoyanData, // 猫眼
+  '36ky': fetch36kyData, // 36氪
+  sspai: fetchSspaiData, // 少数派
+  xinquji: fetchXinqujiData, // 新趣集
 };
 
 app.use(cors());
 
 app.get('/', async (req, res) => {
-	const now = Date.now();
-	const target = req.query.target;
+  const now = Date.now();
+  const target = req.query.target;
 
-	if (!target) {
-		return res.json({
-			code: 1,
-			msg: `support targets: ${Object.keys(targetActionMap).join(', ')}`,
-		});
-	}
+  if (!target) {
+    return res.json({
+      code: 1,
+      msg: `support targets: ${Object.keys(targetActionMap).join(', ')}`,
+    });
+  }
 
-	const targetAction = targetActionMap[target];
+  const targetAction = targetActionMap[target];
 
-	if (!targetAction) {
-		return res.json({ code: 1, msg: `target ${target} is not supported` });
-	}
+  if (!targetAction) {
+    return res.json({ code: 1, msg: `target ${target} is not supported` });
+  }
 
-	if (cache[req.url] && now - cache[req.url].timestamp < 3600000) {
-		// 1h
-		return res.json({ code: 0, list: cache[req.url].data });
-	}
+  if (cache[req.url] && now - cache[req.url].timestamp < 3600000) {
+    // 1h
+    return res.json({ code: 0, list: cache[req.url].data });
+  }
 
-	try {
-		const list = await targetAction();
-		cache[req.url] = { timestamp: now, data: list };
-		res.json({ code: 0, list: list });
-	} catch (err) {
-		console.error(err);
-		res.json({ code: 1, msg: err.message });
-	}
+  try {
+    const list = await targetAction(req.query);
+    cache[req.url] = { timestamp: now, data: list };
+    res.json({ code: 0, list: list });
+  } catch (err) {
+    console.error(err);
+    res.json({ code: 1, msg: err.message });
+  }
 });
 
 app.use((req, res, next) => {
-	next();
+  next();
 });
 
 app.use((err, req, res, next) => {
-	console.error(err);
-	res.json({ code: 1, msg: err.message });
+  console.error(err);
+  res.json({ code: 1, msg: err.message });
 });
 
 process.on('uncaughtException', err => {
-	console.error(err);
-	res.json({ code: 1, msg: err.message });
+  console.error(err);
+  res.json({ code: 1, msg: err.message });
 });
 
 if (process.env.NODE_ENV === 'dev') {
-	app.listen('3000', () => {
-		console.log('Express is running in http://localhost:3000');
-	});
+  app.listen('3000', () => {
+    console.log('Express is running in http://localhost:3000');
+  });
 }
 
 module.exports = app;
